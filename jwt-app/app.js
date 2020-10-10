ff98ce49923a51645eb5e7d0193a5fbb46e1f8c5
G-shino <shinogu2929@gmail.com>
2020-10-10 20:51:42 +0900

    add auth routes

--- jwt-app/app.js
+++ jwt-app/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+// jsonをobjに変換する
+app.use(express.json());
 
 const dbURI = `mongodb+srv://${process.env.USERNAME}:${process.env.USERPW}@cluster0.8bpok.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose
@@ -19,3 +22,4 @@ mongoose
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
